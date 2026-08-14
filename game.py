ee1f0c52e151fe028f7e86092f95e817d9eeff0c
syj399777-coder <syj399777@gmail.com>
2026-08-14 14:19:04 +0900

    데이터 저장 (state.json)

--- game.py
+++ game.py
@@ -40,3 +40,15 @@ class QuizGame:
         self.quizzes = default_data
         self.best_score = 0
         self.save_data()
+
+        # 데이터 저장 (state.json)
+    def save_data(self):
+        try:
+            data = {
+                "quizzes": [q.to_dict() for q in self.quizzes],
+                "best_score": self.best_score
+            }
+            with open(self.filename, "w", encoding="utf-8") as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+        except Exception as e:
+            print(f"데이터 저장 실패: {e}")
